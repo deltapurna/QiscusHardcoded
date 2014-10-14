63ff0a4d0133dfbe7e01d515d9b2105110481994
hiraq <hrxwan@gmail.com>
2014-10-14 07:46:08 +0700

    setup redirect

--- js/main.js
+++ js/main.js
@@ -8,14 +8,31 @@ var qiscus = angular.module('qiscus', [
 ]);
 
 /*
+setup angular routing
+*/
+qiscus.config(function($routeProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'LoginController'
+        })
+        .when('/popup', {
+            templateUrl: 'popup.html',
+            controller: 'ListCommentsController'
+        });
+});
+
+/*
 application main method
 */
-qiscus.run(['$rootScope', '$injector', function($rootScope, $injector) {
+qiscus.run(['$rootScope', '$injector', '$location' ,'$q', function($rootScope, $injector, $location, $q) {
 
     var endpoints = $injector.get('QEndPoints');
     var hardcoded = $injector.get('QHardCoded');
     var login = $injector.get('QServiceLogin');
     var ch = $injector.get('ch');
+
+    var deferred = $q.defer();
 
     /*
     check if already has user_token
@@ -35,9 +52,15 @@ qiscus.run(['$rootScope', '$injector', function($rootScope, $injector) {
                     $rootScope.$broadcast('this_token_value', newVal);
                 }
             });
+
+            deferred.resolve('logged in');
+
+        } else {//if not logged in -> token not set yet
+            deferred.reject('not logged in');
         }
     });
 
     chrome.browserAction.setBadgeText({text: ''});
+    console.log(deferred);
 
 }]);
