fe6affb36d4aed1b0060420467ba889f5d741a43
Delta Purna Widyangga <deltapurna@gmail.com>
2014-10-14 08:17:10 +0700

    successfully updated local storage

--- js/libs/controllers/OptionsController.js
+++ js/libs/controllers/OptionsController.js
@@ -1,18 +1,22 @@
 'use strict';
 
 qiscus.controller('OptionsController', [
-	'QServiceToken',
 	'QHardCoded',
-	'ch', function(qtoken, qhardcoded, ch) {
+	'ch', function(qhardcoded, ch) {
 
-    _this = this;
+    var _this = this;
 		this.roomId = qhardcoded.getRoom();
 		this.topicId = qhardcoded.getTopic();
 
 		this.updateOption = function() {
       qhardcoded.setRoom(_this.roomId);
       qhardcoded.setTopic(_this.topicId);
-      ch.storage.sync.set({'room_id': _this.roomId, 'topic_id': _this.topicId});
+      ch.storage.sync.set({
+        'room_id': _this.roomId, 
+        'topic_id': _this.topicId});
+      ch.storage.sync.get([ 'room_id', 'topic_id' ], function(item){
+        console.log(item);
+      });
 		};
 
 	}
